6cc6e5d687b81186dc66c80d88951d63305eb833
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-18 00:09:46 +0800

    m

--- node_default/istanbul-start.sh
+++ node_default/istanbul-start.sh
@@ -8,6 +8,6 @@ echo "[*] Starting Constellation Node"
 
 echo "[*] Starting Ethereum Node"
 set -v
-ARGS="--syncmode full --mine --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul"
+ARGS="--nodiscover --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul --emicheckpoints"
 PRIVATE_CONFIG=qdata/c/tm.ipc nohup geth --datadir qdata/dd $ARGS --permissioned --rpcport 22000 --port 21000 --unlock 0 --password passwords.txt 2>>qdata/logs/log.log &
 echo "Node configured. See 'qdata/logs' for logs, and run e.g. "geth attach qdata/dd/geth.ipc" to attach to the Geth node."
