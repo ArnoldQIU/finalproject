7f99471c8c6cc12b7eaf799b97c243ffdb67c3c1
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-20 21:42:49 +0800

    config

--- controlscript/create_deployment.sh
+++ controlscript/create_deployment.sh
@@ -25,7 +25,7 @@ spec:
       initContainers:
       - name: init-7node
         image: markpengisme/7node:node
-        command: ['sh', '-c', 'git', 'clone', 'https://github.com/ArnoldQIU/finalproject.git', '/home']
+        command: ['sh', '-c', 'git', 'clone', 'https://github.com/ArnoldQIU/finalproject.git']
         volumeMounts:
         - mountPath: /home/backup
           name: 7node-map
