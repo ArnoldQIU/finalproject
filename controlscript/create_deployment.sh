d08b10b504b68141102a9157346c987b10b9dd7c
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-20 22:13:06 +0800

    config

--- controlscript/create_deployment.sh
+++ controlscript/create_deployment.sh
@@ -25,7 +25,7 @@ spec:
       initContainers:
       - name: init-7node
         image: markpengisme/7node:node
-        command: [\"mkdir\", \"/home/111111\"]
+        command: ['mkdir', '111111']
         volumeMounts:
         - mountPath: /home/backup
           name: 7node-map
