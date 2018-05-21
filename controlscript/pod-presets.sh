034783a3a3b56790b0d0dab8bb04b4636e9181eb
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-22 01:07:58 +0800

    init_container

--- controlscript/pod-presets.sh
+++ controlscript/pod-presets.sh
@@ -4,7 +4,7 @@ for deploy in `seq $NUM_START $NUM_END`
 do 
   IPTEMP_1=$(kubectl get svc nodesvc1 | awk 'NR>1 {print $4}')
   IPTEMP=$(kubectl get svc nodesvc$deploy | awk 'NR>1 {print $4}')
-  echo "apps/v1
+  echo "apiVersion: apps/v1
 kind: PodPreset
 metadata:
   name: 7node-database
