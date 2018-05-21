3034c6a443a265ca6f244de957a1d73e7746d382
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-21 10:46:15 +0800

    redo

--- controlscript/create_deployment.sh
+++ controlscript/create_deployment.sh
@@ -25,7 +25,7 @@ spec:
       initContainers: 
       - name: restart
         image: markpengisme/7node:node 
-        command: ['source']
+        command: ['sh']
         args: ['/home/restart/restart.sh']
         volumeMounts: 
         - mountPath: /home/backup 
