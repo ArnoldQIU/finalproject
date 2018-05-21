8364aca3d59ae80e28a2f3413c6ef421e727ca94
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-22 01:41:17 +0800

    init_container

--- controlscript/pod-presets.sh
+++ controlscript/pod-presets.sh
@@ -4,8 +4,7 @@ for deploy in `seq $NUM_START $NUM_END`
 do 
   IPTEMP_1=$(kubectl get svc nodesvc1 | awk 'NR>1 {print $4}')
   IPTEMP=$(kubectl get svc nodesvc$deploy | awk 'NR>1 {print $4}')
-  echo 
-"apiVersion: settings.k8s.io/v1alpha1
+  echo "apiVersion: settings.k8s.io/v1alpha1
 kind: PodPreset
 metadata:
   name: 7node-database
@@ -15,9 +14,9 @@ spec:
       app: 7node
   env:
     - name: SERVICE_IP
-      value: ${IPTEMP}
+      value: \"${IPTEMP}\"
     - name: SERVICE_IP1
-      value: ${IPTEMP_1}
+      value: \"${IPTEMP_1}\"
   volumeMounts: 
     - mountPath: /home/node_default 
       name: node-default
