a0d0bbf5f1ff233a9a19e2df0cb0a6e006a0f8be
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-28 22:44:15 +0800

    refresh

--- controlscript/deploy.sh
+++ controlscript/deploy.sh
@@ -3,7 +3,7 @@ NUM_END=$2
 for v in `seq $NUM_START $NUM_END`
 do
 	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c \
-	"cd home/node && ./stop.sh"
+	"cd home/node && chmod 755 *.sh && ./stop.sh"
 done
 
 for v in `seq $NUM_START $NUM_END`
