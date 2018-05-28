f0fa25e803e97cb5a3d7f5a3aaa36663094685aa
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-29 01:19:18 +0800

    refresh

--- controlscript/refresh_all_permissioned.sh
+++ controlscript/refresh_all_permissioned.sh
@@ -5,5 +5,15 @@ echo refreshing permissioned-nodes...
 sh controlscript/copy_default.sh $NUM_START $NUM_END
 NUM=$(kubectl get deploy | awk '{print substr($1,5,4)}')
 sh controlscript/generate_permissioned.sh $NUM
-sh controlscript/deploy.sh 1 2 
+for v in `seq $NUM_START $NUM_END`
+do
+	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "cd home/node && chmod 755 *.sh && ./stop.sh"
+done
+
+for v in `seq $NUM_START $NUM_END`
+do
+	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "cd home/node && ./raft-init.sh && ./raft-start.sh" &
+	echo -e "No.$v node deploy ok\n\n"
+done
+
 sh node_default/create_ui.sh 1
