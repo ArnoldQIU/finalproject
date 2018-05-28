bd1179e8824ac772672ac72aeb4182c27df9ac87
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-29 01:08:18 +0800

    refresh

--- controlscript/refresh_all_permissioned.sh
+++ controlscript/refresh_all_permissioned.sh
@@ -5,5 +5,7 @@ echo refreshing permissioned-nodes...
 sh controlscript/copy_default.sh $NUM_START $NUM_END
 NUM=$(kubectl get deploy | awk '{print substr($1,5,4)}')
 sh controlscript/generate_permissioned.sh $NUM
+NUM_START=$1
+NUM_END=$2
 sh controlscript/deploy.sh $NUM_START $NUM_END
 sh node_default/create_ui.sh 1
