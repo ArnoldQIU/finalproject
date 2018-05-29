769762e00656d78aeee5bfff0ad6ad89d2648ff8
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-29 11:38:59 +0800

    refresh

--- controlscript/refresh_all_permissioned.sh
+++ controlscript/refresh_all_permissioned.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 NUM_START=$1
 NUM_END=$2
-DEPLOY1=$3
-DEPLOY2=$4
 echo refreshing permissioned-nodes...
 ## copy passwords	raft-init	raft-start	stop
 ./controlscript/copy_default.sh $NUM_START $NUM_END
@@ -10,6 +8,6 @@ NUM=$(kubectl get deploy | awk '{print substr($1,5,4)}')
 sh controlscript/generate_permissioned.sh $NUM
 echo deplolying...
 sleep 10
-./controlscript/deploy.sh DEPLOY1 DEPLOY2
+./controlscript/deploy.sh $NUM_START $NUM_END
 
 cd /node_default && sh node_default/create_ui.sh 1
